0850ab9a522e3ebf31b074ad3163e70523b48221
matt <mbbyrnes@gmail.com>
2017-11-13 11:15:34 -0800

    first push

---
+++ post_install.sh
@@ -0,0 +1,7 @@
+mkdir /var/www/symfony3/app/logs/dev.log
+chmod 777 /var/www/symfony3/app/logs/dev.log
+
+mkdir /var/www/symfony3/app/cache/dev/twig
+mkdir /var/www/symfony3/app/cache/dev/translations
+mkdir /var/www/symfony3/app/cache/dev/profiler
+chmod -R 777 /var/www/symfony3/app/cache
